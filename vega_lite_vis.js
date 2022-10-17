d0921a63c9545417d6373bce848ec642029e46dc
kngu0058 <80750688+kngu0058@users.noreply.github.com>
2022-10-17 14:05:14 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,24 @@
+var  Graph1 = "Graph1.vg.json";
+vegaEmbed("#Graph1", Graph1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var Graph2 = "Graph2.vg.json";
+vegaEmbed("#Graph2", Graph2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var Graph3 = "Graph3.vg.json";
+vegaEmbed("#Graph3", Graph3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var Graph4 = "Graph4.vg.json";
+vegaEmbed("#Graph4", Graph4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var Graph5 = "Graph5.vg.json";
+vegaEmbed("#Graph5", Graph5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
